b0418c976b6c187b89fe0f7e0b1319f7eb23658c
Albert <albert@Alberts-MacBook-Pro.local>
2021-01-10 18:41:01 +0100

    Switch form .storyboard to .xib

--- PT FDS Albert/SceneDelegate.swift
+++ PT FDS Albert/SceneDelegate.swift
@@ -16,7 +16,17 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+                      
+              let searchViewController = SearchViewController()
+          
+              let navigationController = UINavigationController(rootViewController: searchViewController)
+              
+              window = UIWindow(frame: UIScreen.main.bounds)
+              window?.rootViewController = navigationController
+              window?.windowScene = windowScene
+              window?.makeKeyAndVisible()
+        
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
